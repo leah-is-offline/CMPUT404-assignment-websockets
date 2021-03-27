a54b50f6f25c42a921fbc2d0ea010647a08f45e7
leah-is-offline <copeland@ualberta.ca>
2021-03-27 15:50:10 -0700

    done but sometimes monkeys dont show up - need to do citations

--- sockets.py
+++ sockets.py
@@ -61,27 +61,81 @@ class World:
 
 myWorld = World()        
 
+'''***************************************************************************'''
+'''SRC: https://github.com/abramhindle/WebSocketsExamples/blob/master/chat.py '''
+clients = list()
+
+def send_all(msg):
+    for client in clients:
+        client.put( msg )
+
+def send_all_json(obj):
+    send_all( json.dumps(obj) )
+
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def put(self, v):
+        self.queue.put_nowait(v)
+
+    def get(self):
+        return self.queue.get()
+
+'''***************************************************************************'''
 def set_listener( entity, data ):
     ''' do something with the update ! '''
+    new_entity = {entity: data}
+    send_all_json(new_entity)
 
 myWorld.add_set_listener( set_listener )
         
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    #return None
+    return flask.redirect('static/index.html')
 
+'''***************************************************************************'''
+'''SRC: https://github.com/abramhindle/WebSocketsExamples/blob/master/chat.py '''
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
-    return None
+    # return None
+    try:
+        while True:
+            msg = ws.receive()
+            print("WS RECV: %s" % msg)
+            if (msg is not None):
+                packet = json.loads(msg) 
+                for key in packet:
+                    myWorld.set(key, packet[key])
+            else:
+                break
+    except:
+        print("Done")
 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
     # XXX: TODO IMPLEMENT ME
-    return None
+    # return None
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn( read_ws, ws, client )    
+    try:
+        while True:
+            # block here
+            msg = client.get()
+            ws.send(msg)
+    except Exception as e:# WebSocketError as e:
+        print("WS Error %s" % e)
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
+
+'''***************************************************************************'''
 
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
@@ -96,27 +150,49 @@ def flask_post_json():
     else:
         return json.loads(request.form.keys()[0])
 
+'''***************************************************************************'''
+'''SRC: https://github.com/leah-is-offline/CMPUT404-assignment-ajax/blob/master/server.py '''
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    #return None
+    raw_post_data = flask_post_json()
+    if request.method == "POST":
+        myWorld.set(entity, raw_post_data)
+    if request.method == "PUT":
+        for key in raw_post_data:
+            myWorld.update(entity,key,raw_post_data[key]) #dont return the world - should work for post as well?
+    response = json.dumps(raw_post_data)
+    status_code = 200
+    return response, status_code
+
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    #return None
+    response = json.dumps(myWorld.world()) #RETURN THE WHOLE WORLD
+    status_code = 200
+    return response, status_code
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
-
+    #return None
+    world_entity = myWorld.get(entity)
+    response = json.dumps(world_entity)
+    status_code = 200
+    return response, status_code
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
-
+    #return None
+    myWorld.clear()
+    response = json.dumps(myWorld.world()) 
+    status_code = 200
+    return response,status_code 
+'''***************************************************************************'''
 
 
 if __name__ == "__main__":
